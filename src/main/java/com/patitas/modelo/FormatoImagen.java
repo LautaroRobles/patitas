2208d83dbf671e453782f26d5e1f2738c10481f3
LautaroRobles <lautarorobles_oro@hotmail.com>
2021-06-13 20:16:33 -0300

    Foto y FormatoImagen

---
+++ src/main/java/com/patitas/modelo/FormatoImagen.java
@@ -0,0 +1,5 @@
+package com.patitas.modelo;
+
+public enum FormatoImagen {
+    JPG, PNG
+}
---
+++ src/main/java/com/patitas/modelo/Foto.java
@@ -0,0 +1,8 @@
+package com.patitas.modelo;
+
+public class Foto {
+    private int ancho;
+    private int alto;
+    private String calidad;
+    private FormatoImagen formato;
+}
