10b22e1d4f4b364a4c39d67a7333eac1591731de
Mateo Robaina <62450656+mateorobaina@users.noreply.github.com>
2021-06-15 11:44:51 -0300

    Arrancando rescatista

--- src/main/java/com/patitas/modelo/Rescatista.java
+++ src/main/java/com/patitas/modelo/Rescatista.java
@@ -1,1 +1,13 @@
+package com.patitas.modelo;
 
+import java.util.List;
+import java.util.Date;
+
+public class Rescatista {
+  private String nombre;
+  private String apellido;
+  private Date fechaNacimiento;
+  private String tipoDocumento;
+  private String nroDocumento;
+  private String direccion;
+  private list<Contacto> contactos;}
