049c81aacb6628955e6dbb78a84f73b48dd774f0
LautaroRobles <lautarorobles_oro@hotmail.com>
2021-12-01 18:42:09 -0300

    Pantalla de inicio

--- frontend/src/views/Inicio.vue
+++ frontend/src/views/Inicio.vue
@@ -1,149 +1,44 @@
 <template>
-    <v-container>
-        <v-row class="mt-2">
-            <v-col class="d-flex">
-                <v-row>
-                    <v-col>
-                        <span class="text-h5">Publicaciones</span>
-                    </v-col>
-                    <v-col class="d-flex flex-column-reverse flex-sm-row justify-end">
-                        <v-btn
-                            text
-                            color="primary"
-                            class="mt-2 mt-sm-0 mr-sm-2"
-                            @click="filtros.mostrar = filtros.mostrar === undefined ? 0 : undefined"
-                        >
-                            Filtros
-                            <v-icon v-if="filtros.mostrar === 0" right>mdi-chevron-up</v-icon>
-                            <v-icon v-else right>mdi-chevron-down</v-icon>
-                        </v-btn>
-                        <v-btn color="primary">
-                            Crear publicacion
-                            <v-icon right>mdi-plus</v-icon>
-                        </v-btn>
-                    </v-col>
-                </v-row>
-            </v-col>
-        </v-row>
-        <v-row class="my-0">
-            <v-col>
-                <v-expansion-panels
-                    v-model="filtros.mostrar"
-                    flat
-                    popout
-                >
-                    <v-expansion-panel style="background-color: transparent">
-                        <v-expansion-panel-content>
-                            <v-row>
-                                <v-col>
-                                    <v-select
-                                        outlined
-                                        dense
-                                        label="Categoria"
-                                        :items="categorias"
-                                        v-model="filtros.categoria"
-                                        clearable
-                                        hide-details
-                                    >
-                                    </v-select>
-                                </v-col>
-                                <v-col>
-                                    <v-text-field
-                                        outlined
-                                        dense
-                                        label="Titulo"
-                                        v-model="filtros.titulo"
-                                        hide-details
-                                    ></v-text-field>
-                                </v-col>
-                                <v-col>
-                                    <v-text-field
-                                        outlined
-                                        dense
-                                        label="Cuerpo"
-                                        v-model="filtros.cuerpo"
-                                        hide-details
-                                    ></v-text-field>
-                                </v-col>
-                            </v-row>
-                        </v-expansion-panel-content>
-                    </v-expansion-panel>
-                </v-expansion-panels>
-            </v-col>
-        </v-row>
-        <v-row>
-            <v-col
-                class="col-12 col-sm-6 col-lg-4"
-                v-for="publicacion in publicaciones_filtradas"
-                :key="publicacion.id"
+    <v-container fluid class="fill-height pa-0">
+        <v-carousel
+            show-arrows-on-hover
+            hide-delimiters
+            height="600"
+        >
+            <v-carousel-item
+                v-for="(imagen, index) in imagenes"
+                :key="index"
+                :src="imagen"
             >
-                <v-card>
-                    <v-card-subtitle class="pb-0">{{publicacion.categoria}}</v-card-subtitle>
-                    <v-card-title class="pt-0">{{publicacion.titulo}}</v-card-title>
-                    <v-card-text>{{publicacion.cuerpo}}</v-card-text>
-                    <v-card-actions>
-                        <v-btn text color="primary" small>Ver mas</v-btn>
-                    </v-card-actions>
-                </v-card>
-            </v-col>
-        </v-row>
+            </v-carousel-item>
+        </v-carousel>
+        <div class="fill-height d-flex align-center justify-center bienvenido">
+            <span class="text-h2 white py-2 px-4">
+                Bienvenidos a Patitas
+            </span>
+        </div>
     </v-container>
 </template>
 
 <script>
 export default {
     data: () => ({
-        filtros: {
-            mostrar: undefined,
-            categoria: "",
-            titulo: "",
-            cuerpo: ""
-        },
-        categorias: [
-            {
-                id: 1,
-                value: "Quiero adoptar",
-                text: "Quiero adoptar",
-                descripcion: "Bla bla bla"
-            }
-        ],
-        publicaciones: [
-            {
-                id: 1,
-                titulo: "Lorem ipsum",
-                cuerpo: "aelfjailfjealj fa kejlka elkaefaelkf aela efale",
-                categoria: "Quiero adoptar",
-                organizacion_id: 1,
-                autor_id: 1
-            },
-            {
-                id: 2,
-                titulo: "Abcdefgh",
-                cuerpo: "roiqwpoi pqo iropr iqop rqepoiqer poi",
-                categoria: "Mascota en adopcion",
-                organizacion_id: 1,
-                autor_id: 1
-            },
+        imagenes: [
+            "https://cdn.discordapp.com/attachments/830832256931070035/915632647638765588/perro_-_gato.jpg",
+            "https://cdn.discordapp.com/attachments/830832256931070035/915632415874101248/img_por_que_mi_perro_es_posesivo_con_los_juguetes_20719_600.jpg",
+            "https://cdn.discordapp.com/attachments/830832256931070035/915632338937999401/Purina_Tips_para_elegir_juguetes_para_perros_y_gatos.png",
+            "https://cdn.discordapp.com/attachments/830832256931070035/915632055801491536/50142b7d-674f-4d30-8e20-fcf03fa71eee_16-9-aspect-ratio_default_0.jpg"
         ]
     }),
-    computed: {
-        publicaciones_filtradas() {
-            if(!this.filtros.categoria && !this.filtros.titulo && !this.filtros.cuerpo)
-                return this.publicaciones;
-
-            return this.publicaciones.filter(
-                publicacion =>
-                    (!this.filtros.categoria || publicacion.categoria === this.filtros.categoria) &&
-                    publicacion.titulo.toLowerCase().search(this.filtros.titulo.toLowerCase()) !== -1 &&
-                    publicacion.cuerpo.toLowerCase().search(this.filtros.cuerpo.toLowerCase()) !== -1
-            );
-        }
-    }
 }
 </script>
 
 <style>
-.v-expansion-panel-content__wrap {
-    padding: 0 !important;
+.bienvenido {
+    position: absolute;
+    top:0;
+    left: 0;
+    width: 100%;
+    pointer-events: none;
 }
 </style>
